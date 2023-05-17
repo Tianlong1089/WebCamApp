e010acc055e24a63fdaa0ae4ef0e2c256dd8b515
Tianlong1089 <109485639+Tianlong1089@users.noreply.github.com>
2023-05-17 12:33:32 -0600

    Update camera_app.py
    Added option for uploader file

--- camera_app.py
+++ camera_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from PIL import Image
 
+st.subheader("Color to Grayscale Converter")
+
+uploaded_image = st.file_uploader("Upload Image")
+
 with st.expander('Start camera'):
     #Start camera
     camera_image = st.camera_input("Camera")
@@ -9,7 +13,3 @@ with st.expander('Start camera'):
 if camera_image:
     #Create a pillow image
     img = Image.open(camera_image)
-
-    #Convert pillow image
-    gray_img = img.convert("L")
-    st.image(gray_img)
